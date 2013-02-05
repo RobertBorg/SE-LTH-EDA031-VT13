84eec6c98256b37eb6116808a23eb3ec227929e4
Robert Borg <rauban@gmail.com>
2013-02-05 16:35:16 +0100

    removed trash

--- Labs/lab1/encode.cc
+++ Labs/lab1/encode.cc
@@ -1,14 +1,4 @@
-#include <iostream>
-#include <fstream>
 #include "coding.h"
-
-using std::endl;
-using std::cout;
-using std::ifstream;
-using std::cin;
-using std::istream;
-using std::flush;
-using std::cerr;
 
 int main (int argc, const char* argv[]) {
 	Coding::process( &Coding::encode,  argc, argv);
