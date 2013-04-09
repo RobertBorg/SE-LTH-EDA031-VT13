441efab28e63ffee757e92a4e6d5593d632734bb
Patrik Billgren <dt09pb3@beta-3.student.lth.se>
2013-04-09 10:11:38 +0200

    added files

---
+++ project/src/wireprotocol/client_messagehandler.h
@@ -0,0 +1,13 @@
+#ifndef CLIENT_MESSAGEHANDLER_H__
+#define CLIENT_MESSAGEHANDLER_H__
+
+#include "iostream_news.h"
+
+class ClientMessageHandler {
+public:
+	shared_ptr<Package> parsePkg(iostream_news& in) {
+		
+	}
+};
+
+#endif /* end of include guard: CLIENT_MESSAGEHANDLER_H__ */
---
+++ project/src/wireprotocol/server_messagehandler.h
@@ -0,0 +1,13 @@
+#ifndef SERVER_MESSAGEHANDLER_H__
+#define SERVER_MESSAGEHANDLER_H__
+
+#include "iostream_news.h"
+
+class ServerMessageHandler {
+public:
+	shared_ptr<Package> parsePkg(iostream_news& in) {
+		
+	}
+};
+
+#endif /* end of include guard: SERVER_MESSAGEHANDLER_H__ */
