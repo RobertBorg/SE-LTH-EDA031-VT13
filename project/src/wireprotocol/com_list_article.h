9204e388446cfeaac8e8183eaf4f3515a87e7c69
Patrik Billgren <dt09pb3@beta-3.student.lth.se>
2013-04-09 11:38:53 +0200

    again

---
+++ project/src/wireprotocol/com_list_article.h
@@ -0,0 +1,36 @@
+#ifndef COM_LIST_ARTICLE_H__
+#define COM_LIST_ARTICLE_H__
+
+
+class ComListArticlePackage : public ComPackage{
+public:
+	AnsPackage process(Database *db){
+		
+	}
+	ComListArticlePackage(int groupNum_): groupNum(groupNum_){}
+
+
+private:
+	int groupNum;
+};
+
+
+istream_news& operator>>(istream_news &in, ComListPackage &rhs) {
+	in.eat(Protocol::COM_LIST_ART);
+	num_p num;
+	in >> num;
+	groupNum = num;
+	in.eat(Protocol::COM_END);
+	return in;
+}
+
+iostream_news& operator<<(iostream_news &out, ComListPackage &rhs) {
+	out << Protocol::COM_LIST_ART;
+	out << num_p(groupNum);
+	out << Protocol::COM_END;
+	return out;
+}
+
+
+
+#endif
