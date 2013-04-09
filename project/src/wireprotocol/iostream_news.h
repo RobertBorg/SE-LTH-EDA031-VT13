5d604417314824d01a448779adf416bcafa9ca5a
Patrik Billgren <dt09pb3@beta-3.student.lth.se>
2013-04-09 11:18:26 +0200

    listpackage

--- project/src/wireprotocol/iostream_news.h
+++ project/src/wireprotocol/iostream_news.h
@@ -5,7 +5,13 @@
 
 class iostream_news : public iostream {
 public:
-
+	void eat(const uint8_t &expects){
+		uint8_t next;
+		&this << next;
+		if (next != expects){
+			throw ProtocolViolationException();
+		}
+	}
 };
 
 #endif /* end of include guard: NEWSSTREAM_H__ */
--- project/src/wireprotocol/list_package.h
+++ project/src/wireprotocol/list_package.h
@@ -5,15 +5,21 @@
 class ComListPackage : public ComPackage{
 public:
 	AnsPackage process(Database *db){
-		// go through database and list EVERYTHING
+		
 	}
 };
 
 istream_news& operator>>(istream_news &in, ComListPackage &rhs) {
-
-
+	in.eat(Protocol::COM_LIST_NG);
+	in.eat(Protocol::COM_END);
+	return in;
 }
 
+iostream_news& operator<<(iostream_news &out, ComListPackage &rhs) {
+	out << Protocol::COM_LIST_NG;
+	out << Protocol::COM_END;
+	return out;
+}
 
 
 #endif
