558bf5dfc2b602a927f4c7919211bd32e3811054
Patrik Billgren <patrik.billgren@lunicore.se>
2013-04-10 15:48:20 +0200

    InMemoryDatabase

--- project/src/database/newsgroup.h
+++ project/src/database/newsgroup.h
@@ -8,6 +8,41 @@ public:
 	vector<Article> articles;
 	string name;
 	uint32_t id; // Must be unique
+
+	uint32_t articleCounter = 1; // Describes the number of the next article
+
+
+	void addArticle(shared_ptr<Article> article){
+		article->id = articleCounter;
+		articles.emplace(article->id, article);
+		ngIt->articleCounter++;
+	}
+	shared_ptr<Article> getArticle(uint32_t artId){
+		auto artIt = articles.find(artId);
+		if (artIt == articles.end){
+			throw ArtDoesntExistException();
+		}
+		return artIt;
+	}
+
+	void deleteArticle(uint32_t artId){
+		auto artIt = articles.find(artId);
+		if (artIt == articles.end){
+			throw ArtDoesntExistException();
+		}
+		articles.erase(artIt);
+	}
+
+	const auto getArticleIterator(){
+		return articles.cbegin();
+	}
+	const auto getArticleEnd(){
+		return articles.cend();
+	}
+
+
+private:
+	map<uint32_t, shared_ptr<Article>> articles;
 };
 
 #endif
