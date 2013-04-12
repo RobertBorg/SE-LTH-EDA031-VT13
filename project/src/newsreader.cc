d836f9ee950531415f589a7c1d6f66ad31b3a2a8
Robert Borg <rauban@gmail.com>
2013-04-12 11:58:19 +0200

    proper parsing of option + short version for address and port

--- project/src/newsreader.cc
+++ project/src/newsreader.cc
@@ -9,13 +9,15 @@ using client_server::Connection;
 int main(int argc, char* argv[]) {
 	ClientOptions o(argc,argv);
 	o();
-	std::string adr = o["server-address"].as<std::string>();
-	uint16_t port = o["server-port"].as<uint16_t>();
+	if( o.count("server-address") && o.count("server-address")) {
+		std::string adr = o["server-address"].as<std::string>();
+		uint16_t port = o["server-port"].as<uint16_t>();
+		Connection *connection = new Connection(adr.c_str(), port);
+		cout << adr << ":" << port << endl;
+	}
 
 
-	Connection *connection = new Connection(adr.c_str(), port);
 
 
-	cout << adr << ":" <<port << endl;
 	return 0;
 }
