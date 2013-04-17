67ca44a00943cca960d293e78ebb18b3ba98208b
Christofer Heimonen <sane@ubuntu.(none)>
2013-04-17 14:58:58 +0200

    Test file updated

--- project/build/ClientTests.sh
+++ project/build/ClientTests.sh
@@ -14,85 +14,78 @@ die () {
 [ "$#" -eq 1 ] || die "1 argument required, $# provided"
 echo $1 | grep -E -q '^[0-9]+$' || die "Numeric argument required, $1 provided"
 
-echo "Showing help:"
+echo "- Showing help:"
 
-#./newsreader --server-port $1 --server-address localhost --help
-echo "Issue created, doesn't work atm"
+./newsreader --server-port $1 --server-address localhost --help
 
-echo "Listing newsgroups when there are no newsgroups:"
+echo "- Listing newsgroups when there are no newsgroups:"
 
 ./newsreader --server-port $1 --server-address localhost list-newsgroup
 
-echo "Creating newsgroup:"
+echo "- Creating newsgroup:"
 
 ./newsreader --server-port $1 --server-address localhost create-newsgroup MyNewsGroup
 
-echo "Listing newsgroups:"
+echo "- Listing newsgroups:"
 
 ./newsreader --server-port $1 --server-address localhost list-newsgroup
 
-echo "Deleting the previously created newsgroup:"
+echo "- Deleting the previously created newsgroup:"
 
 ./newsreader --server-port $1 --server-address localhost delete-newsgroup 1
 
-echo "Creating a new newsgroup:"
+echo "- Creating a new newsgroup:"
 
 ./newsreader --server-port $1 --server-address localhost create-newsgroup Test
 
-echo "Attempting to delete a newsgroup that doesn't exist:"
+echo "- Attempting to delete a newsgroup that doesn't exist:"
 
 ./newsreader --server-port $1 --server-address localhost delete-newsgroup 5
 
-echo "Listing articles from the newsgroup when there are no articles:"
+echo "- Listing articles from the newsgroup when there are no articles:"
 
 ./newsreader --server-port $1 --server-address localhost list-article 2
 
-echo "Creating an article:"
+echo "- Creating an article:"
 
 ./newsreader --server-port $1 --server-address localhost create-article 2 Test 'Christofer Heimonen' 'lalal lalallalalal lalal lalal all allal al lalala lala'
 
-echo "Listing all articles from the newsgroup:"
-#./newsreader --server-port $1 --server-address localhost list-article 2
-echo "Issue created, doesn't work atm"
+echo "- Listing all articles from the newsgroup:"
+./newsreader --server-port $1 --server-address localhost list-article 2
 
-echo "Listing articles from a newsgroup that doesn't exist:"
+echo "- Listing articles from a newsgroup that doesn't exist:"
 
-#./newsreader --server-port $1 --server-address localhost list-article 6
-echo "Issue created, doesn't work atm"
+./newsreader --server-port $1 --server-address localhost list-article 6
 
-echo "Fetching the article:"
+echo "- Fetching the article:"
 
 ./newsreader --server-port $1 --server-address localhost get-article 2 1
 
-echo "Deleting article:"
+echo "- Deleting article:"
 
 ./newsreader --server-port $1 --server-address localhost delete-article 2 1
 
-echo "Deleting article that doesn't exist:"
+echo "- Deleting article that doesn't exist:"
 
 ./newsreader --server-port $1 --server-address localhost delete-article 2 6
 
-echo "Deleting article from a newsgroup that doesn't exist:"
+echo "- Deleting article from a newsgroup that doesn't exist:"
 
 ./newsreader --server-port $1 --server-address localhost delete-article 9 1
 
-echo "Creating an article into a newsgroup that doesn't exist:"
+echo "- Creating an article into a newsgroup that doesn't exist:"
 
-# ./newsreader --server-port $1 --server-address localhost create-article 10 Test 'Christofer Heimonen' 'lalal lalallalalal'
-echo "Issue created, doesn't work atm"
+./newsreader --server-port $1 --server-address localhost create-article 10 Test 'Christofer Heimonen' 'lalal lalallalalal'
 
-echo "Fetching an article that doesn't exist:"
+echo "- Fetching an article that doesn't exist:"
 
-#./newsreader --server-port $1 --server-address localhost get-article 2 7
-echo "Issue created, doesn't work atm"
+./newsreader --server-port $1 --server-address localhost get-article 2 7
 
-echo "Fetching an article from a newsgroup that doesn't exist:"
-#./newsreader --server-port $1 --server-address localhost get-article 7 1
-echo "Issue created, doesn't work atm"
+echo "- Fetching an article from a newsgroup that doesn't exist:"
+./newsreader --server-port $1 --server-address localhost get-article 7 1
 
-echo "Sending a command that isn't supported:"
+echo "- Sending a command that isn't supported:"
 
-#./newsreader --server-port $1 --server-address localhost trueThat
-echo "Issue created, doesn't work atm"
+./newsreader --server-port $1 --server-address localhost trueThat
 
 exit 0
